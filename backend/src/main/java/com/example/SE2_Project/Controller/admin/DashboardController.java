44ce2710f1364bf4cbe02a582b36b739cda6e3e6
Vin Com <0plmzxc@gmail.com>
2025-03-25 22:55:18 +0700

    Complete homepage and admin

--- backend/src/main/java/com/example/SE2_Project/Controller/admin/DashboardController.java
+++ backend/src/main/java/com/example/SE2_Project/Controller/admin/DashboardController.java
@@ -16,24 +16,36 @@ public class DashboardController {
     public String getCategoryPage(){
         return "admin/category/categoryPage";
     }
+    @GetMapping("/category/add")
+    public String getCategoryAddPage(){
+        return "admin/category/add";
+    }
     @GetMapping("/category/update/{id}")
     public String getUpdatePage(@PathVariable("id") Long id){
-        return "admin/category/update/"+id;
+        return "admin/category/update";
     }
     @GetMapping("/transaction")
     public String getTransactionPage(){
         return "admin/transaction/transactionList";
     }
+    @GetMapping("/transaction/add")
+    public String getTransactionAddPage(){
+        return "admin/transaction/add";
+    }
     @GetMapping("/transaction/update/{id}")
     public String getTransactionUpdatePage(@PathVariable("id") Long id){
-        return "admin/transaction/update/"+id;
+        return "admin/transaction/update";
     }
     @GetMapping("/user")
     public String getUserPage(){
         return "admin/user/userList";
     }
+    @GetMapping("/user/add")
+    public String getUserAddPage(){
+        return "admin/user/add";
+    }
     @GetMapping("/user/update/{id}")
     public String getUserUpdatePage(@PathVariable("id") Long id){
-        return "admin/user/update"+id;
+        return "admin/user/update";
     }
 }
